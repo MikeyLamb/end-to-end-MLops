e777f07e29a7c469fab5c73d4ca8a3a4dc9dd30a
MikeyLamb <mslambrecht@gmail.com>
2024-02-26 09:06:52 +0200

    Update requirements.txt

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
+
 def get_requirements(file_path:str)->List[str]:
     '''
     this function returns the list of requirements
