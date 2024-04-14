12719cfb0b4fe5575a162c2e280f38c365c24f1d
MikeyLamb <mslambrecht@gmail.com>
2024-04-14 09:12:18 +0200

    Create logger and exception

---
+++ src/logger.py
@@ -0,0 +1,25 @@
+import logging
+import os
+from datetime import datetime
+from exception import CustomException
+import sys
+
+log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",log_file)
+os.makedirs(logs_path, exist_ok=True)
+
+log_file_path=os.path.join(logs_path,log_file)
+
+logging.basicConfig(
+    filename=log_file_path,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO, 
+    
+)
+
+if __name__=="__main__":
+    try:
+        y = 1/0
+    except Exception as e:
+        logging.info("Logging has started")
+        raise CustomException(e, sys)
